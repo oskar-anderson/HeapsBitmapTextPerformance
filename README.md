d6effa414c7ea1ffc22a6a54c9e83e49d4f3c48c
oskar-anderson <andersonkarloskar@gmail.com>
2022-08-25 14:12:42 +0300

    text rendering

---
+++ README.md
@@ -0,0 +1,22 @@
+# Heaps Text performance test
+
+
+## hl
+```
+haxe ./compile_hl.hxml
+hl ./hello.hl
+```
+
+## html5
+1. Compile
+```
+haxe ./compile_html5.hxml
+```
+2. Run
+* a. Open index.html in the browser through the file protocol.
+* b. Click 'Go Live' button with Live Server VSCode extension.
+
+
+## Tutorials/Samples/API
+[Official](https://heaps.io/documentation/) - 
+[Deepnight](https://deepnight.net/tutorials/)
--- src/Main.hx
+++ src/Main.hx
@@ -1,9 +1,63 @@
-class Main extends hxd.App {
+import h3d.Vector;
+import hxd.Res;
+import hxd.Rand;
+import hxd.App;
+
+class Main extends App {
+    var h2dTextBoard = [];
+    var frameCount = 0;
+    var h2dTextframeCount = null;
+
     override function init() {
-        var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        tf.text = "Hello World !";
+        var font : h2d.Font = hxd.res.DefaultFont.get();
+
+        Res.initEmbed();
+        var ps2pFont = hxd.Res.fonts.ps2p_14_text_32bit.toFont();
+
+        h2dTextframeCount = new h2d.Text(ps2pFont, s2d);
+        h2dTextframeCount.text = Std.string(frameCount);
+        
+        var board = getBoard();
+        for (y in 0...100) {
+            var row = [];
+            for (x in 0...100) {
+                var text = new h2d.Text(ps2pFont, s2d);
+                text.text = board[y][x];
+                text.x = x * 14;
+                text.y = y * 14 + 14;
+                row.push(text);
+            }
+            h2dTextBoard.push(row);
+        }
     }
+
+    override function update(dt:Float) {
+        frameCount++;
+        h2dTextframeCount.text = Std.string(frameCount);
+
+        var board = getBoard();
+        for (y in 0...100) {
+            for (x in 0...100) {
+                h2dTextBoard[y][x].text = board[y][x];
+            }
+        }
+    }
+
     static function main() {
         new Main();
+    }
+
+    public static function getBoard(): Array<Array<String>> {
+        var board:Array<Array<String>> = [for (y in 0...100) [for (x in 0...100) null]];
+        var randomPool = "ABCDEFGHIJKLMNOPQRSTUVWabcdefghijklmnopqrstuvw";
+        var random = Rand.create();
+        for (y in 0...100) {
+            for (x in 0...100) {
+                var letter = randomPool.charAt(random.random(randomPool.length));
+                board[y][x] = letter;
+            }
+        }
+        
+        return board;
     }
 }
